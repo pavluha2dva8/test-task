45b413d908df34346b7daf73ed018cc0dc7f157a
Pavlo Kolomiiets <pkolomiiets@rightandabove.com>
2022-12-30 17:34:15 +0200

    Refactor QnA interface & render component

--- src/pages/refactor-2/refactor-2.tsx
+++ src/pages/refactor-2/refactor-2.tsx
@@ -1,27 +1,23 @@
 import { CenteredLayout } from '~/components/layouts';
 
-// TODO refactor
-
-interface QuestionOrAnswer {
-  question?: string;
-  answer?: string;
+interface QuestionAndAnswer {
+  question: string;
+  answer: string;
 }
 
-const QnA: QuestionOrAnswer[] = [
-  { question: 'Do you run like a fish?' },
-  { answer: 'Absolutely man' },
-  { question: 'Have you tried to swim like a dinosaur?' },
-  { answer: 'Nah, not my cup of tea' },
-  { question: 'How are we counting from 5 to 10?' },
-  { answer: 'Do I look like a counter?' },
+const QnA: QuestionAndAnswer[] = [
+  { question: 'Do you run like a fish?', answer: 'Absolutely man' },
+  { question: 'Have you tried to swim like a dinosaur?', answer: 'Nah, not my cup of tea' },
+  { question: 'How are we counting from 5 to 10?', answer: 'Do I look like a counter?' },
 ];
 
-const QnaRender = ({ question, answer }: QuestionOrAnswer) => {
-  if (question) {
-    return <h3 className="font-bold text-lg">{question}</h3>;
-  } else {
-    return <p className="mb-2">{answer}</p>;
-  }
+const QnaRender = ({ question, answer }: QuestionAndAnswer) => {
+  return (
+    <>
+      <h3 className="font-bold text-lg">{question}</h3>
+      <p className="mb-2">{answer}</p>
+    </>
+  );
 };
 
 export const Refactor2 = () => {
